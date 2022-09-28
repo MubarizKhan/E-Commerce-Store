0cbeaeb231e102db0038251e17f41df7e69b4e18
MubarizKhan <p176007@nu.edu.pk>
2022-09-28 17:20:20 +0500

    callback serial_id generator in Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,13 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
 
+  after_commit :create_Product_serialID, on: :create
+
+  def create_Product_serialID
+
+    s_id =  "Product--" + (Product.count).to_s
+    # topic_status = 'Topic Status Updated!'
+    update_column(:serial_id, s_id)
+
+  end
 end
