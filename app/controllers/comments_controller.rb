a2398faab3d7297cc140fcb83dffff35f5e7589d
MubarizKhan <p176007@nu.edu.pk>
2022-09-29 12:25:19 +0500

    working on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,35 @@
 class CommentsController < ApplicationController
+  def create
+    # if user_signed_in?
+
+    @product = current_user.products.find(params[:product_id])
+    @comment = @product.comments.create(comment_params)
+    redirect_to article_path(@article)
+    # end
+  end
+
+  # def edit
+  #   @product = current_user.products.find(params[:id])
+  #   @comment = @product.comments.find(params[:product_id])
+  # end
+
+  # def update
+  #   @product = current_user.products.find(params[:product_id])
+  #   @comment = @product.comments.find(params[:id])
+
+  #   if @comment.update(comment_params)
+  #     flash[:notice] = 'You updated your comment!'
+  #     @comment.save
+  #     redirect_to root_path
+  #   else
+  #     render :edit
+  #   end
+  # end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
 end
