521ce1db502ec9c136380712ee0a5cd46a902fd0
MubarizKhan <p176007@nu.edu.pk>
2022-09-30 14:26:14 +0500

    ...

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,10 @@
 class CartsController < ApplicationController
+  def index
+    print "--------------------"
+    print "--------------------"
+    print "---------INDEX CART-----------"
+    print "--------------------"
+    print "--------------------"
+    @cart = Cart.new(user_id :current_user:id)
+  end
 end
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,2 +1,9 @@
 class LineItemsController < ApplicationController
+  def index
+    print "--------------------"
+    print "--------------------"
+    print "---------Line Items-----------"
+    print "--------------------"
+    print "--------------------"
+  end
 end
