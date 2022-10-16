438e2fd29cc458a4d8167a2421dfb8bcfd3af0b5
MubarizKhan <p176007@nu.edu.pk>
2022-10-17 01:28:56 +0500

    order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,22 @@
 # frozen_string_literal: true
 
+
 class OrdersController < ApplicationController
+  before_action :set_creation_order, only: %i[show active_order update order ]
+
   def index
     @orders = current_user.orders.all
     authorize @orders
   end
 
   def show
-
-    @order = Order.find(params[:id])
+    # @order = Order.find(params[:id])
     @order_line_items = LineItem.where(order_id: @order.id)
     # authorize @order
   end
 
   def active_order
-    @order = Order.find(params[:id])
-    # @order = current_user.orders.last
+    # @order = Order.find(params[:id])
     @order_line_items = LineItem.where(order_id: @order.id)
   end
 
@@ -25,19 +26,25 @@ class OrdersController < ApplicationController
   end
 
   def update
-    @order = Order.find(params[:id])
+    # @order = Order.find(params[:id])
     @order.update(coupon_name: params[:order][:coupon_name])
   end
 
   def order
-    # @order = Order.find_by(user_id: current_user.id, status: 'adding_items')
-    @order = Order.find(params[:id])
+
     price = OrdersManager::OrderManager.new(order: @order).call
     redirect_to order_path(@order.id)
-    # render :show
   end
+
+  private
 
   def order_params
     params.permit(:user_id)
   end
+
+  def set_creation_order
+    @order = Order.find(params[:id])
+  end
+
+
 end
