0d750443b0e45fc81a043c771078d551ebfdb698
MubarizKhan <p176007@nu.edu.pk>
2022-10-17 15:22:42 +0500

    adding before_action: authenticate_user!

--- app/services/OrdersManager/order_manager.rb
+++ app/services/OrdersManager/order_manager.rb
@@ -1,42 +1,53 @@
+# frozen_string_literal: true
 
 module OrdersManager
-
   class OrderManager
-
     def initialize(order:)
       @order = order
       # @cart = cart_id
     end
 
     def call
-      result = order
+      order!
+      # result = {}
+      # {order: order!, errors: @errors}
     end
 
     private
 
-    def order
-      LineItem.where(order_id: @order.id).all.each do |item|
-
+    def order!
+      # LineItem.where(order_id: @order.id).each do |item|
+      @order.line_items.each do |item|
         product = Product.find(item.product_id)
         product.item_quantity -= item.quantity
-        product.save
+        if product.save && item.update(cart_id: nil)
+          print  "+++++++()()()+++++"
+          print  "+++++++()()()+++++"
+          print  "+++++++()()()+++++"
 
-        item.update(cart_id: nil)
+          apply_coupon_discount!
+        else
+          @errors[product: product&.errors&.full_messages&.join(', ')]
+          begin
+            @errors[item: item.errors.full_messages.join(', ')]
+          rescue StandardError
+            nil
+          end
+        end
       end
-      coupon_discount
+
       @order.completed!
     end
 
-    def coupon_discount
-      @coupon = Coupon.find_by(coupon_name: @order.coupon_name)
-      if @coupon
-        discounted_price = @order.order_amount - (@order.order_amount * @coupon.discount)
+    def apply_coupon_discount!
+      coupon = Coupon.find_by(coupon_name: @order.coupon_name)
+      if coupon
+        discounted_price = @order.order_amount - (@order.order_amount * coupon.discount)
         @order.update(order_amount: discounted_price)
       else
-        @order
+        # add error message here
+        # @order
       end
-
     end
-
   end
 end
